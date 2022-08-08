e68017adf5c2b3a4f71f24fa14f0b4b74ab2e36b
Mohammad Bagher Ehtemam <ehtemam@gmail.com>
2022-08-08 17:53:39 +0000

    feat: solve ex 6 form

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,27 +4,34 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const inputRef = React.useRef(null);
+  const [isValid, setIsValid] = React.useState(true);
+  const [value, setValue] = React.useState('');
+  function handleSubmit(event){
+    event.preventDefault();
+    // const username = event.target.elements[0].value;
+    const username = inputRef.current.value;
+    onSubmitUsername(username);
+  }
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const onChange = (event) => {
+    const { value } = event.target;
+    // if(value === value.toLowerCase()){
+    //   setIsValid(true);
+    // }else {
+    //   setIsValid(false);
+    // }
+    setValue(value.toLowerCase());
+  }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor='username'>Username:</label>
+        <input id='username' onChange={onChange} value={value} type="text" ref={inputRef} />
+        {/* {!isValid &&  <p role='alert'> value must be lowercase</p> } */}
       </div>
+      {/* <button type="submit" disabled={!isValid}>Submit</button> */}
       <button type="submit">Submit</button>
     </form>
   )
